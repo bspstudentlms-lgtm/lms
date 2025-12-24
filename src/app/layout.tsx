ce2ef422782f5728bcab18b9e76dcd7f16c6de90
Sivak <sivak@avetti.ca>
2025-12-24 13:24:30 +0530

    Home page changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,36 @@ import ConditionalHeader from "../layout/ConditionalHeader";
 const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Your App",
+  title: "Backstage Pass LMS | Online Learning Platform",
+  description:
+    "Backstage Pass LMS offers live classes, recorded courses, quizzes, mentor support, and certificates for skill development.",
+
+  openGraph: {
+    title: "Backstage Pass LMS | Learn. Practice. Get Certified",
+    description:
+      "A complete Learning Management System with live classes, recorded videos, quizzes, mentor support, and certificates.",
+    url: "https://lms-bsp.vercel.app/",
+    siteName: "Backstage Pass LMS",
+    images: [
+      {
+        url: "https://backstagepass.co.in/newlogo-324ee245.webp",
+        width: 1200,
+        height: 630,
+        alt: "Backstage Pass LMS",
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Backstage Pass LMS | Online Learning Platform",
+    description:
+      "Learn with expert mentors through live classes, videos, quizzes, and certifications.",
+    images: ["https://backstagepass.co.in/newlogo-324ee245.webp"],
+  },
 };
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
