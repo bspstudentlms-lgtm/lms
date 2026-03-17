c05cd08eee61516b6c4cf175a2d78cc1af4dd5d3
gayathri291 <gayathrig74@gmail.com>
2026-03-17 14:58:42 +0530

    added meta title,description dynamically

--- src/app/[path]/page.jsx
+++ src/app/[path]/page.jsx
@@ -1,10 +1,33 @@
 import CoursePage from "@/components/CoursePage";
 
-export const metadata = {
-  title: "Game Development Course | Backstage Pass",
-  description:
-    "Learn game development, 3D modeling, and design with expert-led courses from Backstage Pass.",
-};
+export async function generateMetadata({ params }) {
+  try {
+    const res = await fetch(
+      `https://backstagepass.co.in/reactapi/api/course_innerpage.php?path=${params.path}`,
+      { cache: "no-store" } // important if content changes
+    );
+
+    const data = await res.json();
+
+    return {
+      title: data?.meta_title || data?.course_name || "Game Development Course | Backstage Pass",
+      description:
+        data?.meta_description ||
+        "Learn game development, 3D modeling, and design with expert-led courses from Backstage Pass.",
+      openGraph: {
+        title: data?.meta_title || data?.course_name,
+        description: data?.meta_description,
+       
+      },
+    };
+  } catch (error) {
+    return {
+      title: "Game Development Course | Backstage Pass",
+      description:
+        "Learn game development, 3D modeling, and design with expert-led courses from Backstage Pass.",
+    };
+  }
+}
 
 export default function Page({ params }) {
   return <CoursePage params={params} />;
