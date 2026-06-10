54ff03f96b6ba435191bf3c0ca93e11e03e41830
Siva Prasad <katurisiva51@gmail.com>
2026-06-10 10:20:41 +0530

    Test Google SSO Chat Session6

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -37,6 +37,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const login = (userData: User) => {
     setUser(userData);
     localStorage.setItem("user", JSON.stringify(userData));
+      window.location.reload();
   };
 
   const logout = () => {
