dfc70eeecd2f1b67faf16c8c34fb273745787a77
Durga@1995r <katuridurga551@gmail.com>
2026-08-14 11:40:32 +0530

    durga removed s in courses for testing

--- src/components/AllCoursesPageContent.tsx
+++ src/components/AllCoursesPageContent.tsx
@@ -182,9 +182,9 @@ const HomePageContent = () => {
 
       <div className="best-cpurse section-padding">
         <div className="container">
-          <div className="section-title">
+          <div className="section-title durga-test">
             <h2>Popular Courses</h2>
-            <p>Choose Our <span>Top Courses</span></p>
+            <p>Choose Our <span>Top Course</span></p>
           </div>
 
 
